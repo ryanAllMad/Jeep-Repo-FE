15f9d35dbf309287f759f5eafa8431c2200ec4cd
Mykol Benning <mykolbenning@mykols-mbp.myfiosgateway.com>
2021-02-13 16:23:25 -0500

    data displaying site two

--- jeep-prices/src/components/Container.js
+++ jeep-prices/src/components/Container.js
@@ -9,7 +9,7 @@ const Container = () => {
     return(
         <div>
             <SiteOne />
-            {/* <SiteTwo /> */}
+            <SiteTwo />
         </div>
     )
 }
--- jeep-prices/src/components/SiteTwo.js
+++ jeep-prices/src/components/SiteTwo.js
@@ -1,40 +1,63 @@
 import '../styles/sitetwo.scss';
+import React, {useState, useEffect} from 'react';
 //importing arrow icons
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 
 const SiteTwo = () => {
+
+    const [siteTwoState, setSiteTwoState] = useState([]);
+
+
+    useEffect(() => {
+        axios.get('https://jeep-prices-repo-be.herokuapp.com/siteTwo')
+        .then(res => setSiteTwoState(res.data))
+        // .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+    }, [])
+
+
     return(
-        <div className='site-container'>
-        <FontAwesomeIcon icon="angle-left" className='left-arrow-two' />
-        <h2>Site two goes here.</h2>
+        <>
+        {siteTwoState.map((data, index) => {
+            return(
+                <div className='site-container'>
+                <FontAwesomeIcon icon="angle-left" className='left-arrow-two' />
+                <h2>Site two goes here.</h2>
 
 
-        <div className='miles-container'>
-        <h3>Mileage</h3>
-        <p>Mileage Placeholder</p>
-        </div>
+                <div className='miles-container'>
+                <h3>Mileage</h3>
+                <p>{data.mileage}</p>
+                </div>
 
-        <div className='listPrice-container'>
-        <h3>List Price</h3>
-        <p>List Price Placeholder</p>
-        </div>
+                 <div className='listPrice-container'>
+                 <h3>List Price</h3>
+                 <p>{data.listPrice}</p>
+                 </div>
 
-        <div className='monthlyPrice-container'>
-        <h3>Monthly Price</h3>
-        <p>Monthly Price Placeholder</p>
-        </div>
+                <div className='monthlyPrice-container'>
+                <h3>Monthly Price</h3>
+                <p>{data.monthlyPrice}</p>
+                </div>
 
-        <div className='test'>
-            <h3>Vehicle Details</h3>
-            <p>Vehicle Details Here</p>
-        </div>
+                <div className='test'>
+                <h3>Vehicle Details</h3>
+                <p>{data.deetz}</p>
+                </div>
 
-        <div className='vehicleImg-container'>
-            <h3>Car Img</h3>
-        </div>
+                <div className='vehicleImg-container'>
+                <h3>Car Img</h3>
+                <p>{data.img}</p>
+                </div>
 
-        <FontAwesomeIcon icon="angle-right" className='right-arrow-two' />
-        </div>
+                <FontAwesomeIcon icon="angle-right" className='right-arrow-two' />
+                </div>
+
+
+            )
+        })}
+        </>
     )
 }
 
